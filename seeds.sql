6cbf3bb98ea19be55df935dcb4cd254f4c06745e
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-14 17:31:42 -0600

    initial database schema, removed bamazon.js

---
+++ seeds.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+
+USE bamazonDB;
+
+
+CREATE TABLE products (
+     item_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+     product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY(item_id)
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES( "Nintendo Switch", "Electronics", 299.00, 500);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Arctic Mokeys: AM", "Digital Music", 12.99, 1000);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Digital Delay Pedal", "Musical Instrument Accessories", 179.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Shimmer pedal", "Musical Instrument Accessories", 179.99, 20);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Sweater Dress", "Womens' clothing", 20.99, 55);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Plaid - boyfriend fit", "Womens' clothing", 25.00, 100);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Gibson Les Paul Studio T", "Musical Instruments", 750.00, 15);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Scott Pilgrim VS the World", "Digital Movies", 19.00, 15);
+
+
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Arctic Mokeys: AM", "Digital Music", 12.99, 1000);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Plaid - boyfriend fit", "Womens' clothing", 25.00, 100);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Embroidered Jacket", "Womens' clothing", 55.00, 120);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Bandana", "Accessories", 2.50, 120);
