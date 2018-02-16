21943c18f4c9196fed0b0c78841efe63074ed9e2
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-16 17:48:55 -0600

    initial database queries

---
+++ admin.js
@@ -0,0 +1,2 @@
+//in this file i have the queries written out 
+const mySQL = require('mySQL');
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const inquirer = require('inquirer');
 const mySQL = require('mySQL');
+// const Admin = require('/.admin');
 
 // startApp();
 // promptUserForProudctId()
 // processOrder() 
 //condition here 
-//if sufficent, then placeOrder()
+//if sufficent, then checkout();
 //updateDatabaseQuantity()
 //else console.log(not enough) and go back to prompt user to productID
 //at the end, promptUser for productID
@@ -25,45 +26,60 @@ var connection = mySQL.createConnection({
 //make sure connection is successful
 connection.connect(function(err){
     if (err) throw err;
-    console.log(`Connected as ${connection.threadId}`);
+    // console.log(`Connected as ${connection.threadId}`);
     afterConnection();
 });
-
 
 function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
        console.log(res);
+       start();
        //keeping the close connection function for now
-       connection.end();
+    //    connection.end();
     });
 }
-
 
 //here is the start function -we want this to execute AFTER database is connected 
 
 function start() {
     inquirer.prompt([
         {
-            name: "ProductID",
+            name: "productID",
             type: "list",
             message: "Please select a product ID for what you would like to buy.",
             choices: ["1-Nintendo Switch", "2-Arctic Monkeys: AM", "3-Digital Delay Pedal", "4-Sweater Dres", "5-Plaid: Boyfriend Fit", "6-Gibson Les Paul Studio T", "7-Shimmer Pedal", "8-Scott Pilgrim Vs The World", "9-Embroidered Jacket", "10-bandana"]
         },
 
         { 
-            name: "ProductQuantity",
+            name: "productQuantity",
             type: "input",
-            message: "How many do you want?"
-
-
+            message: "How many would you like?"
         }
+        //confirm they want it /read terms and conditions 
+        //ask if they want anything else, or go to checkout        
     ]).then(function(answers){
         console.log("nice choice!");
         //store user query in a var 
-        //and use that va
-
+        //and use that var
+        //later on, keep asking if they want anything else and then do a reduce function
+        let productID = answers.productID;
+        let productQuantity = answers.productQuantity;
+        console.log("Please confirm: You would like to purchase " + productQuantity + productID);
+       let query = "SELECT product_name, price FROM products WHERE item_id = ? ";
+       console.log(query);
+       connection.query(query, answers.productID, function(err, response){
+           if (err) throw err;
+           console.log(response[1].product_name);
+        //    console.log(
+        //        "ProductID:  " + id +
+        //        " || Product Quantity: " 
+        //    );
+       })
     });
 }
 
-start();
+//the following are being moved to admin.js file 
+// processOrder();
+// checkOut();
+// start();
