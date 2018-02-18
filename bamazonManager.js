dbe0a682065abcd5be26d9f840f606e2941b2a30
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-18 13:28:09 -0600

    added pseudocode for manager.js file

---
+++ bamazonManager.js
@@ -0,0 +1,44 @@
+//  dependencies  - may need to require bamazon and call the files from there too 
+const inquirer = require('inquirer');
+const mySQL = require('mySQL');
+
+//data base connecting 
+//  pseudocode  -this will be elaborted on and fixed 
+
+switch {
+    case viewProductsForSale:
+        viewProductsForSale();
+        break;
+    case viewLowInventory:
+        viewLowInventory();
+        break;
+    case addNewProduct:
+         addNewProduct();
+         break;
+    case addToInventory:
+        addToInventory();
+        break;
+    default:
+        start();
+ }
+
+
+ function viewProductsForSale() {
+
+
+ }
+
+
+ function viewLowInventory() {
+
+ }
+
+ function addToInventory() {
+
+ }
+
+
+ function addNewProduct() {
+     
+ }
+
